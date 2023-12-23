a35a53d5503680ffe00cea8792e8b68ac25ae739
Sofia <sofiatselenx@gmail.com>
2023-12-23 13:11:56 +0200

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,8 +1,6 @@
 function getElementWidth(content, padding, border) {
     return (
-        Number.parseFloat(content) +
-        Number.parseFloat(padding) * 2 
-        Number.parseFloat(border) * 2
+        Number.parseFloat(content) + Number.parseFloat(padding) * 2 + Number.parseFloat(border) * 2
     );
 }
 console.log(getElementWidth("50px", "8px", "4px")); // 74
